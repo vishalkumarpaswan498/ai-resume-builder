817a126483b98f7890788b5960573c2504cc5059
vishalkumarpaswan498 <vkumarpaswan498@gmail.com>
2026-08-14 12:14:23 +0530

    Prepare backend for Render

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(
+    `🚀 Server is running on port ${PORT}`
+  );
 });
